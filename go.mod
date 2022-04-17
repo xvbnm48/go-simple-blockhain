72730b8a87036d65c1410a6d087d202f87c51203
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-04-17 23:47:15 +0700

    make route with mux

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/xvbnm48/go-simple-blockhain
+
+go 1.17
+
+require github.com/gorilla/mux v1.8.0
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", getBlockchain).Methods("GET")
+	r.HandleFunc("/", writeBlock).Methods("POST")
+	r.HandleFunc("/new", newBook).Methods("POST")
+
+	log.Println("listening on port 3000")
+
+	log.Fatal(http.ListenAndServe(":3000", r))
+}
