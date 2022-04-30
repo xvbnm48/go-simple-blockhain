846b7bfed69e70fbb380eae91086d42fcf3c8b53
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-04-30 22:45:01 +0700

    update func newBook

--- main.go
+++ main.go
@@ -1,17 +1,85 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type Block struct {
+	Pos       int
+	Data      BookCheckout
+	TimeStamp string
+	Hash      string
+	PrevHash  string
+}
+
+type BookCheckout struct {
+	BookID       string `json:"book_id"`
+	User         string `json:"user"`
+	CheckoutDate string `json:"checkout_date"`
+	IsGenesis    bool   `json:"is_genesis"`
+}
+
+type Book struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Author      string `json:"author"`
+	PublishDate string `json:"publishDate"`
+	ISBN        string `json:"isbn"`
+}
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+var Blockchain *Blockchain
+
+func writeBlock(w http.ResponseWriter, r *http.Request) {
+	var checkoutitem BookCheckout
+
+	if err := json.NewDecoder(r.Body).Decode(&checkoutitem); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("could not write block: %v", err)
+		w.Write([]byte("could not write block"))
+	}
+}
+
+func Newbook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+
+	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("could not decode book: %v", err)
+		w.Write([]byte("could not decode book"))
+		return
+	}
+
+	h := md5.New()
+	io.WriteString(h, book.ISBN+book.PublishDate)
+	book.ID = fmt.Sprintf("%x", h.Sum(nil))
+
+	resp, err := json.MarshalIndent(book, "", " ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("could not marshal payload:%v", err)
+		w.Write([]byte("could not marshal payload"))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", getBlockchain).Methods("GET")
 	r.HandleFunc("/", writeBlock).Methods("POST")
-	r.HandleFunc("/new", newBook).Methods("POST")
+	r.HandleFunc("/new", Newbook).Methods("POST")
 
 	log.Println("listening on port 3000")
 
---
+++ readme.md
@@ -0,0 +1,3 @@
+this is tutorial from [ahlil sarma]([https://link](https://youtu.be/19zTYCAQRZg))
+
+last learn at 43:27
